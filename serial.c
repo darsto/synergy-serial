c8a38b973e6962d96cb470dce80a9936a49584e2
Darek Stojaczyk <darek.stojaczyk@gmail.com>
2022-05-15 18:19:28 +0200

    serial: Implement the arduino protocol

--- serial.c
+++ serial.c
@@ -65,3 +65,57 @@ serial_set_blocking(int should_block)
 
 	if (tcsetattr(g_fd, TCSANOW, &tty) != 0) perror("tcsetattr");
 }
+
+struct serial_msg {
+	char tag[4];
+	uint16_t arg1;
+	uint16_t arg2;
+	uint16_t arg3;
+	uint16_t arg4;
+};
+
+static int
+serial_sendmsg(struct serial_msg *msg)
+{
+	return write(g_fd, msg, sizeof(*msg));
+}
+
+int
+serial_ard_set_mouse_pos(uint16_t x, uint16_t y)
+{
+	serial_sendmsg(&(struct serial_msg){ "MSET", x, y });
+}
+
+int
+serial_ard_mouse_move(int16_t x_delta, int16_t y_delta)
+{
+	serial_sendmsg(&(struct serial_msg){ "MMOV", x_delta, y_delta });
+}
+
+int
+serial_ard_mouse_down(uint8_t id)
+{
+	serial_sendmsg(&(struct serial_msg){ "MBDN", id });
+}
+
+int
+serial_ard_mouse_up(uint8_t id)
+{
+	serial_sendmsg(&(struct serial_msg){ "MBUP", id });
+}
+
+int
+serial_ard_mouse_wheel(int16_t x_delta, int16_t y_delta)
+{
+	serial_sendmsg(&(struct serial_msg){ "MWHL", x_delta, y_delta });
+}
+
+int serial_ard_key_down(uint16_t id, uint16_t mods, uint16_t phys_id)
+{
+	serial_sendmsg(&(struct serial_msg){ "KBDN", id, mods, phys_id });
+}
+
+int serial_ard_key_up(uint16_t id, uint16_t mods, uint16_t phys_id)
+{
+	serial_sendmsg(&(struct serial_msg){ "KBUP", id, mods, phys_id });
+}
--- serial.h
+++ serial.h
@@ -15,7 +15,7 @@ int serial_ard_set_mouse_pos(uint16_t x, uint16_t y);
 int serial_ard_mouse_move(int16_t x_delta, int16_t y_delta);
 int serial_ard_mouse_down(uint8_t id);
 int serial_ard_mouse_up(uint8_t id);
-int serial_ard_mouse_wheel(uint16_t x_delta, uint16_t y_delta);
+int serial_ard_mouse_wheel(int16_t x_delta, int16_t y_delta);
 int serial_ard_key_down(uint16_t id, uint16_t mods, uint16_t phys_id);
 int serial_ard_key_up(uint16_t id, uint16_t mods, uint16_t phys_id);
 
